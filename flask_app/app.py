1daa95435d9b8f26bb50adba6def18822484c97e
washim <mdwashimmomin04x@gmail.com>
2025-10-07 11:34:53 +0530

    minor debuged

--- flask_app/app.py
+++ flask_app/app.py
@@ -20,7 +20,7 @@ repo_name = "mlops-min-project"
 
 # # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
-dagshub.init(repo_owner=repo_owner, repo_name=repo_name, mlflow=True)
+
 
 
 app = Flask(__name__)
